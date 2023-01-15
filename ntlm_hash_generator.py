5a9a4f64ce561f7d1d7b84dbfdc1286ba071c4b1
MdTasnimKabirAyman <119742549+MdTasnimKabirAyman@users.noreply.github.com>
2023-01-15 06:37:51 +0600

    Add files via upload

---
+++ ntlm_hash_generator.py
@@ -0,0 +1,13 @@
+import hashlib
+
+print('Enter "break" as input to exit the script')
+def ntlm_hash(string):
+    m = hashlib.new('md4', string.encode('utf-16le')).digest()
+    return m.hex().upper()
+
+if __name__=='__main__':
+    input_str = input("Enter the string to generate the NTLM hash: ")
+    while input_str!='break':
+        print(ntlm_hash(input_str))
+        input_str = input("Enter the string to generate the NTLM hash: ")
+    
